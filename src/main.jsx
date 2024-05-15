304f6225d6f020328bc22217a384e0d4a347e834
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-05-15 17:08:01 -0500

    implementando un useMemo inicial para no renderizar cuando se modifican las props del padre

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import ReactDOM from "react-dom/client";
 // import { SimpleForm } from "./02-useEfect/SimpleForm.jsx";
 //import { FormWidthCustomHook } from "./02-useEfect/FormWidthCustomHook.jsx";
 //import { MultipleCustomHooks } from "./03-example/MultipleCustomHooks";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+//import { FocusScreen } from "./04-useRef/FocusScreen";
 import "./index.css";
+import { Memorize } from "./06-memos/Memorize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <FocusScreen />
+  <Memorize />
   // </React.StrictMode>
 );
