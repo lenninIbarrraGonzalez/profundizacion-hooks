bcc005954cac6685958c05bb55e67adc9e9b0094
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-05-15 11:23:26 -0500

    useRef: utilizamos el hook useRef para hacer referencia a un input y enviar el foco

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import ReactDOM from "react-dom/client";
 // import { CounterWhitHooks } from "./01-useState/CounterWithHooks.jsx";
 // import { SimpleForm } from "./02-useEfect/SimpleForm.jsx";
 //import { FormWidthCustomHook } from "./02-useEfect/FormWidthCustomHook.jsx";
-import { MultipleCustomHooks } from "./03-example/MultipleCustomHooks";
+//import { MultipleCustomHooks } from "./03-example/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <MultipleCustomHooks />
+  <FocusScreen />
   // </React.StrictMode>
 );
