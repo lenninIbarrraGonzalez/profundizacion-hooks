12be5e549c6457a0265675321fc0a478cedcd64f
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-05-18 16:35:40 -0500

    creando el cascaron de la lista de TODOS

--- src/08-useReduce/TodoApp.jsx
+++ src/08-useReduce/TodoApp.jsx
@@ -18,13 +18,40 @@ export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
   return (
     <>
-      <h1>TODOS</h1>
+      <h1>
+        TODOS (10), <small>pendientes: 2</small>
+      </h1>
       <hr />
-      <ul>
-        <li>Todo 1</li>
-        <li>Todo 2</li>
-        <li>Todo 3</li>
-      </ul>
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group">
+            {todos.map(() => (
+              <li
+                key={todos.id}
+                className="list-group-item d-flex justify-content-between"
+              >
+                <span className="align-self-center">Item 1</span>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="¿Que hay para hacer?"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-2">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
