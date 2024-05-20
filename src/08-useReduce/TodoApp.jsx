f40b34f5ed7d840488e05c19fa5848e9a68a5caf
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-05-20 18:14:57 -0500

    guardando los TODOS en el localStorage

--- src/08-useReduce/TodoApp.jsx
+++ src/08-useReduce/TodoApp.jsx
@@ -1,23 +1,34 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
 import { TodoList } from "./TodoList";
 import { TodoAdd } from "./TodoAdd";
 
-const initialState = [
-  {
-    id: new Date().getTime(),
-    description: "Lavar la ropa",
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: "Sacar a pasear al perro",
-    done: false,
-  },
-];
+// const initialState = [
+//   {
+//     id: new Date().getTime(),
+//     description: "Lavar la ropa",
+//     done: false,
+//   },
+//   {
+//     id: new Date().getTime() * 3,
+//     description: "Sacar a pasear al perro",
+//     done: false,
+//   },
+// ];
+
+//para el manejo de localStorage
+const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("TODOS")) || [];
+};
 
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("TODOS", JSON.stringify(todos));
+  }, [todos]);
 
   const handleNewTodo = (todo) => {
     //console.log({ todo });
